1d67eaffe7522a42f2404f1a88a182caffd72b5e
canikwe <c.anikwe@gmail.com>
2019-01-08 22:02:33 -0500

    Completed 8JAN19

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,9 @@
 ## code your solution here. 
+class Cat
+    #used attr_accessor to allow the ability to read/write the name instance variable
+   attr_accessor :name
+    
+    def meow
+        puts "meow!"
+    end
+end
